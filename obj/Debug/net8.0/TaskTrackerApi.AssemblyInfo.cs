16858b946aaf8910ba4926d41479a76fc2c124fd
Emre Yılmaz <emreyilmaz@Emres-MacBook-Pro.local>
2025-05-27 01:43:32 +0300

    feat: add GitHub developer avatars to index.html

--- obj/Debug/net8.0/TaskTrackerApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTrackerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f059d7e9fea904e6079e3794f69f4cee35f5469d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6fa311a9e3315722a79e6fec2266ff27d2e8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
