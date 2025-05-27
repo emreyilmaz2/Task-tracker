21e19e1042ba803bd851d41ee4904f03d6d01a8d
Emre Yılmaz <emreyilmaz@Emres-MacBook-Pro.local>
2025-05-28 02:19:19 +0300

    Remove duplicate content includes

--- TaskTrackerApi/obj/Debug/net8.0/TaskTrackerApi.AssemblyInfo.cs
+++ TaskTrackerApi/obj/Debug/net8.0/TaskTrackerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2240f4a7ab06a2d274d7462ec4757ae21bb48fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de13312fb972fbef7b3548ef3a217ac05cec364")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
